f026daecf8fbc44462510979e3d8ef7c36533940
tunderdomb <tunderdomb@gmail.com>
2015-12-03 12:34:46 +0100

    index files

---
+++ index.js
@@ -0,0 +1,10 @@
+var Model = require("./Model")
+var Slice = require("./Slice")
+var collection = require("./collection")
+var schema = require("./schema")
+
+module.exports = Model
+
+Model.Slice = Slice
+Model.collection = collection
+Model.schema = schema
